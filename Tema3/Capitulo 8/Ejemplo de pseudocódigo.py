b294ab17a7b559176e5fe51a6e14761a73539045
gabriel <gabrielalabarcemuro@gmail.com>
2025-03-25 12:42:18 +0100

    Contenido Capitulo 8

--- Tema3/Capitulo 8/Ejemplo de pseudocódigo.py
+++ Tema3/Capitulo 8/Ejemplo de pseudocódigo.py
@@ -2,10 +2,22 @@
 palabra = "oculto"
 
 #Ahora la letra que introducirá el usuario
-letra_introducida = input('Introduce una letra: ').lower
+while True:
+
+    letra_introducida = input("Introduce una letra(introduzca la palabra 'salir' para salir del programa: ").lower()
 
 #Hacemos la comprobación de si la letra introducida está dentro de la palabra oculta
-if letra_introducida in palabra:
-        print('Tu letra SÍ está dentro de la palabra oculta.')
-else:
-        print('Tu letra NO está dentro de la palabra introducida')
+    if letra_introducida in palabra:
+            print("Tu letra SÍ está dentro de la palabra oculta.")
+
+    elif letra_introducida == 'salir':
+        break
+
+
+    else:
+            print("Tu letra NO está dentro de la palabra introducida")
+
+    if letra_introducida == palabra:
+        print('Enhorabuena, usted a adivinado la palabra')
+        break
+
